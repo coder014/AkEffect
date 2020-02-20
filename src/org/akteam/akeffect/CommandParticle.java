196e5bd17260edd82d1f66de990cd96b90352751
coder014 <5218575+coder014@user.noreply.gitee.com>
2020-02-20 11:30:54 +0800

    Optimized particle spawning procedure

--- src/org/akteam/akeffect/CommandParticle.java
+++ src/org/akteam/akeffect/CommandParticle.java
@@ -1,7 +1,25 @@
+/*
+ * AkEffect
+ * Copyright (C) 2020 coder014
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.akteam.akeffect;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -14,89 +32,83 @@ import org.bukkit.util.Vector;
 import java.util.*;
 
 public class CommandParticle implements CommandExecutor {
-    public final HashMap<UUID, Particle> particle4Player = new HashMap<>();
-    private static final Random rnd = new Random();
-    private List<Particle> particleValues = new ArrayList<>();
-    private JavaPlugin plugin;
-    private BukkitRunnable task;
-
-    CommandParticle(JavaPlugin plugin) {
-        this.plugin = plugin;
-        for (Particle p : Particle.values()) {
-            if (p.getDataType() == Void.class) {
-                particleValues.add(p);
-            }
-        }
-        startParticleTask();
-    }
-
-    public void setParticleForPlayer(Player p) {
-        particle4Player.put(p.getUniqueId(), particleValues.get(rnd.nextInt(particleValues.size())));
-    }
-
-    public Particle getParticleForPlayer(Player p) {
-        if (!particle4Player.containsKey(p.getUniqueId())) {
-            setParticleForPlayer(p);
-        }
-
-        return particle4Player.get(p.getUniqueId());
-    }
-
-    public boolean startParticleTask() {
-        if (task == null) {
-            task = new BukkitRunnable() {
-                @Override
-                public void run() {
-                    for (Player p : Bukkit.getOnlinePlayers()) {
-                        Vector d = p.getLocation().getDirection();
-                        d.multiply(-1);
-                        p.getWorld().spawnParticle(getParticleForPlayer(p), p.getLocation().clone().add(d), 2, 0.25, 0.0, 0.25);
-                    }
-                }
-            };
-            task.runTaskTimer(plugin, 10L, 2L);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
-        if (!label.equals("particle4p")) return false;
-        if (args.length <= 0) {
-            if (startParticleTask()) {
-                commandSender.sendMessage("[AkEffect] Particle4Player is now running.");
-            } else {
-                task.cancel();
-                task = null;
-                commandSender.sendMessage("[AkEffect] Particle4Player is now stopped.");
-            }
-        } else switch (args[0]) {
-            case "change":
-                if (commandSender instanceof Player) {
-                    setParticleForPlayer((Player) commandSender);
-                    commandSender.sendMessage("[AkEffect] Particle for you has changed.");
-                } else {
-                    commandSender.sendMessage("[AkEffect] This command must be executed as a player");
-                }
-                break;
-            case "start":
-                if (startParticleTask()) {
-                    commandSender.sendMessage("[AkEffect] Particle4Player is now running.");
-                } else commandSender.sendMessage(ChatColor.YELLOW + "[AkEffect] Particle4Player is already running!");
-                break;
-            case "stop":
-                if (task != null) {
-                    task.cancel();
-                    task = null;
-                    commandSender.sendMessage("[AkEffect] Particle4Player is now stopped.");
-                } else commandSender.sendMessage(ChatColor.YELLOW + "[AkEffect] Particle4Player is already stopped!");
-                break;
-            default:
-                commandSender.sendMessage("[AkEffect] unknown sub command.");
-        }
-
-        return true;
-    }
+	private final Map<String, Integer> particleForPlayer = new HashMap<>();
+	private final Random rnd = new Random();
+	private final List<Particle> particles = new ArrayList<>();
+	private final JavaPlugin plugin;
+	private BukkitRunnable task;
+	
+	public CommandParticle(JavaPlugin plugin) {
+		this.plugin = plugin;
+		for (Particle particle : Particle.values()) {
+			if (particle.getDataType() == Void.class) {
+				particles.add(particle);
+			}
+		}
+		startParticleTask();
+	}
+	
+	public void setParticleForPlayer(Player player) {
+		particleForPlayer.put(player.getName(), rnd.nextInt(particles.size()));
+	}
+		
+	public Particle getParticleForPlayer(Player player) {
+		if (!particleForPlayer.containsKey(player.getName())) {
+			setParticleForPlayer(player);
+		}
+		return particles.get(particleForPlayer.get(player.getName()));
+	}
+	
+	public void startParticleTask() {
+		task = new BukkitRunnable() {
+			@Override
+			public void run() {
+				for (Player player : Bukkit.getOnlinePlayers()) {
+					Location l = player.getLocation();
+					Vector d = l.getDirection().multiply(-1);
+					l.add(d);
+					//spawn particles behind the player
+					player.getWorld().spawnParticle(getParticleForPlayer(player), l, 2, 0.25, 0.25, 0.25);
+				}
+			}
+		};
+		task.runTaskTimer(plugin, 10L, 2L);
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+		if (args.length <= 0) {  //toggle particles
+			if (task.isCancelled()) {
+				startParticleTask();
+				commandSender.sendMessage("[AkEffect] Particle4Player is now running.");
+			} else {
+				task.cancel();
+				commandSender.sendMessage("[AkEffect] Particle4Player is now stopped.");
+			}
+		} else switch (args[0]) {
+			case "change":
+				if (commandSender instanceof Player) {
+					setParticleForPlayer((Player) commandSender);
+					commandSender.sendMessage("[AkEffect] Particle for you has changed.");
+				} else {
+					commandSender.sendMessage("[AkEffect] This command must be executed as a player");
+				}
+				break;
+			case "start":
+				if (task.isCancelled()) {
+					startParticleTask();
+					commandSender.sendMessage("[AkEffect] Particle4Player is now running.");
+				} else commandSender.sendMessage(ChatColor.YELLOW + "[AkEffect] Particle4Player is already running!");
+				break;
+			case "stop":
+				if (!task.isCancelled()) {
+					task.cancel();
+					commandSender.sendMessage("[AkEffect] Particle4Player is now stopped.");
+				} else commandSender.sendMessage(ChatColor.YELLOW + "[AkEffect] Particle4Player is already stopped!");
+				break;
+			default:
+				return false;
+		}
+		return true;
+	}
 }
